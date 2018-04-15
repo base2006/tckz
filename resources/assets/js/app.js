d9ee60210c77bb47bbc827b45f059394bc0a18d6
Bas Hendriks <bbehendriks@gmail.com>
2018-04-15 17:25:37 +0200

    Created sidebar submenu added animation and styling for submenus

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -28,12 +28,34 @@ require('datatables.net-buttons-bs4');
 // });
 
 $(document).ready(function () {
+    $("#sidebar .submenu").css({
+        "height": 0,
+        "opacity": 0
+    });
     // active menu item
-    $("#sidebar .nav a").each(function() {
+    $("#sidebar .nav a").each(function () {
         if (this.href == window.location.href) {
             $(this).parent().addClass("active");
         }
     });
+    $("#sidebar .active .submenu").css({
+        "height": "auto",
+        "opacity": 1
+    });
+
+    $("#sidebar .nav-item").not('.active').hover(function () {
+        var submenu = $(this).find(".submenu");
+        var amount = submenu.children('li').length * 55;
+        submenu.stop().animate({
+            "height": amount+"px",
+            "opacity": 1
+        }, 300);
+    }, function () {
+        $(this).find(".submenu").stop().animate({
+            "height": 0,
+            "opacity": 0
+        });
+    }, 300);
 
 
 });
